38ecaa0065c34d09d1bd5b3928ca21ed399eef2f
Kishor Naik <kishor.naik011.net@gmail.com>
2023-12-11 20:52:27 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/SalesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sol_Demo.Entity;
+using Sol_Demo.Infrastructure;
+using Sol_Demo.Utility;
+
+namespace Sol_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class SalesController : ControllerBase
+    {
+        private readonly AdventureWorks2012Context context;
+
+        public SalesController(AdventureWorks2012Context context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("get-sales-order")]
+        public async Task<IActionResult> GetSalesOrderData([FromQuery] PageQueryParamter pageQuery)
+        {
+            var owners = await PageList<SalesOrderDetail>.ToPagedListAsync(context.SalesOrderDetails, pageQuery.PageNumber, pageQuery.PageSize);
+
+            return base.Ok(await Task.FromResult(owners.ToList()));
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Utility/PageList.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sol_Demo.Utility;
+
+public class PageList<T> : List<T>
+{
+    public int CurrentPage { get; private set; }
+    public int TotalPages { get; private set; }
+    public int PageSize { get; private set; }
+    public int TotalCount { get; private set; }
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
+
+    public PageList(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        TotalCount = count;
+        PageSize = pageSize;
+        CurrentPage = pageNumber;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        AddRange(items);
+    }
+
+    public static async Task<PageList<T>> ToPagedListAsync(IQueryable<T> source, int pageNumber, int pageSize)
+    {
+        Task<List<T>> itemsTask = Task.FromResult(source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList());
+        Task<int> countTask = Task.FromResult(source.Count());
+
+        var counter = await countTask.ConfigureAwait(false);
+        var items = await itemsTask.ConfigureAwait(false);
+
+        return new PageList<T>(items, counter, pageNumber, pageSize);
+    }
+}
